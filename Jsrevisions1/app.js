3bf2cb77e95ed92d97b17aa34c7cb97d8aea78dd
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2025-07-08 08:11:05 +0500

    js revisions Variables overview

---
+++ Jsrevisions1/app.js
@@ -0,0 +1,23 @@
+// VARIABLES
+console.log("Welcome To JavaScript")
+let firstname="Zain"
+let lastname="Ali Shah"
+let country="Pakistan"
+let city="karachi"
+let age=21
+console.log(firstname, lastname, country, city, age) 
+// Day 1 Exercise
+// 1.Write a single line comment which says, comments can make code readable
+
+// 2.Write another single comment which says, 
+// Welcome to 30DaysOfJavaScript
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+var myAge=21
+var yourAge=20
+console.log(myAge,yourAge)
+
+
+
+
