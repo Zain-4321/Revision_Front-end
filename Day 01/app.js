ae75b0b94af35b29ebf1a7aecbfca43f7a159e6b
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2025-08-05 22:14:52 +0500

    js Day 01 Q/A

---
+++ Day 01/app.js
@@ -0,0 +1,94 @@
+// 🟡 Q1: Declare a variable called name and assign your name to it.
+let name="Zain Ali Shah"
+console.log(name);
+//🟡 Q2: Declare a number variable age with any age value.
+
+const age=21
+console.log(age);
+
+// 🟡 Q3: What will this expression return? 10 + "5"
+// there answers is 105 becuse one in integr and other is string
+
+//🟡 Q4: Make a function that returns the square of a number.
+
+function sqr(num1,num2){
+
+    return num1*num2
+
+}
+console.log(sqr(5,5));
+
+// 🟡 Q5: Create a function called greet() that prints "Hello World".
+function greet(str){
+    return str
+}
+
+console.log(greet("Hello World"));
+
+// 🟡 Q6: Write an if-else block that checks if a number is greater than 10 or not.
+let num=11
+
+if (num>10){
+    console.log("Its right");
+    
+}else{
+    console.log("You are Wrong");
+    
+}
+
+
+// 🟡 Q7: Use typeof to check the type of "Hello" and 123.
+let str1="Hello"
+let num1=123
+console.log(typeof(str1)); //string
+console.log(typeof(num1));  //number
+
+// 🟡 Q8: Use a for loop to print numbers from 1 to 5.
+
+for(let i=1;i<6;i++){
+    console.log(i);
+    
+}
+
+//🟡 Q9: Create an array of 3 favorite colors.
+
+let arr=["yellow","green","black"]
+
+console.log(arr);
+//🟡 Q10: Access the first color from your color array.
+console.log(arr[0]);
+
+// 🟡 Q11: Use push() to add another color to your array.
+arr.push("White")
+console.log(arr);
+
+// 🟡 Q12: Create a function that adds two numbers.
+
+function add(num3,num4){
+    return num3+num4
+}
+
+console.log(add(25,25));
+
+// 🟡 Q13: What is the output of true + false?
+//Both are Bolean Values their output will be boolean
+
+//🟡 Q14: Write a function that returns whether a number is even.
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(isEven(8)); // Even
+
+console.log(even());
+
+// 🟡 Q15: What will typeof null return?
+//it will be null hi hoga
+
+
+//typeof null ➝ "object"
+
+
